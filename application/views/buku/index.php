a2d0039ec6dd18b9ec629ac7c273b4d645801d20
naufalfkhri <naufal5068@gmail.com>
2022-11-28 01:13:36 +0700

    tugas 11 selesai

--- application/views/buku/index.php
+++ application/views/buku/index.php
@@ -1,17 +1,16 @@
-<!-- Begin Page Content -->
+<!-- Begin Page Content --> 
 <div class="container-fluid">
 
     <?= $this->session->flashdata('pesan'); ?>
     <div class="row">
         <div class="col-lg-12">
             <?php if(validation_errors()){?>
-            <div class="alert alert-danger" role="alert">
-                <?= validation_errors();?>
-            </div>
+                <div class="alert alert-danger" role="alert">
+                    <?= validation_errors();?>
+                </div>
             <?php }?>
             <?= $this->session->flashdata('pesan'); ?>
-            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#bukuBaruModal"><i
-                    class="fasfa-file-alt"></i> Buku Baru</a>
+            <a href="" class-"btn btn-primary mb-3" data-toggle="modal" data-target="#bukuBaruModal"><i class="fas fa-file-alt"></i> Buku Baru</a>
             <table class="table table-hover">
                 <thead>
                     <tr>
@@ -30,8 +29,8 @@
                 </thead>
                 <tbody>
 
-                    <?php 
-                        $a = 1; 
+                    <?php
+                        $a = 1;
                         foreach ($buku as $b) { ?>
                     <tr>
                         <th scope="row"><?= $a++; ?></th>
@@ -46,16 +45,11 @@
                         <td>
                             <picture>
                                 <source srcset="" type="image/svg+xml">
-                                <img src="<?= base_url('assets/img/upload/') . $b['image'];?>"
-                                    class="img-fluid img-thumbnail" alt="...">
-                            </picture>
-                        </td>
+                                <img src="<?= base_url('assets/img/upload/') . $b['image'];?>" class="img-fluid img-thumbnail" alt="..">
+                            </picture></td>
                         <td>
-                            <a href="<?= base_url('buku/ubahBuku/').$b['id'];?>" class="badge badge-info"><i
-                                    class="fas fa-edit"></i> Ubah</a>
-                            <a href="<?= base_url('buku/hapusbuku/').$b['id'];?>"
-                                onclick="return confirm('Kamu yakin akan menghapus <?= $judul.' '.$b['judul_buku'];?> ?');"
-                                class="badge badge-danger"><i class="fas fa-trash"></i> Hapus</a>
+                            <a href="<?= base_url('buku/ubahBuku/').$b['id'];?>" class="badge badge-info"><i class="fas fa-edit"></i> Ubah</a>
+                            <a href="<?= base_url('buku/hapusbuku/').$b['id'];?>" onclick="return confirm('Kamu yakin akan menghapus <?= $judul.''.$b['judul_buku'];?> ?');" class="badge badge-danger"><i class="fas fa-trash"></i> Hapus</a>
                         </td>
                     </tr>
                     <?php } ?>
@@ -63,15 +57,15 @@
             </table>
         </div>
     </div>
+
 </div>
 <!-- /.container-fluid -->
 
 </div>
 <!-- End of Main Content -->
 
-<!-- Modal Tambah buku baru-->
-<div class="modal fade" id="bukuBaruModal" tabindex="-1" role="dialog" aria-labelledby="bukuBaruModalLabel"
-    aria-hidden="true">
+<!-- Modal Tmabah buku baru-->
+<div class="modal fade" id="bukuBaruModal" tabindex="-1" role="dialog" aria-labelledby="bukuBaruModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -83,54 +77,48 @@
             <form action="<?= base_url('buku'); ?>" method="post" enctype="multipart/form-data">
                 <div class="modal-body">
                     <div class="form-group">
-                        <input type="text" class="form-control form-control-user" id="judul_buku" name="judul_buku"
-                            placeholder="Masukkan Judul Buku">
+                        <input type="text" class="form-control form-control-user" id="judul_buku" name="judul_buku" placeholder="Masukkan Judul Buku">
                     </div>
                     <div class="form-group">
                         <select name="id_kategori" class="form-control form-control-user">
                             <option value="">Pilih Kategori</option>
-                            <?php 
+                            <?php
                             foreach ($kategori as $k) { ?>
-                            <option value="<?= $k['id'];?>"><?= $k['kategori'];?></option>
+                                <option value="<?= $k['id'];?>"><?= $k['kategori'];?></option>
                             <?php } ?>
                         </select>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control form-control-user" id="pengarang" name="pengarang"
-                            placeholder="Masukkan nama pengarang">
+                        <input type="text" class="form-control form-control-user" id="pengarang" name="pengarang" placeholder="Masukkan nama pengarang">
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control form-control-user" id="penerbit" name="penerbit"
-                            placeholder="Masukkan nama penerbit">
+                        <input type="text" class="form-control form-control-user" id="penerbit" name="penerbit" placeholder="Masukkan nama penerbit">
                     </div>
                     <div class="form-group">
                         <select name="tahun" class="form-control form-control-user">
                             <option value="">Pilih Tahun</option>
-                            <?php 
+                            <?php
                             for ($i=date('Y'); $i > 1000 ; $i--) { ?>
-                            <option value="<?= $i;?>"><?= $i;?></option>
+                                <option value="<?= $i;?>"><?=$i;?></option>
                             <?php } ?>
                         </select>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control form-control-user" id="isbn" name="isbn"
-                            placeholder="Masukkan ISBN">
+                        <input type="text" class="form-control form-control-user" id="isbn" name="isbn" placeholder="Masukkan ISBN">
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control form-control-user" id="stok" name="stok"
-                            placeholder="Masukkan nominal stok">
+                        <input type="text" class="form-control form-control-user" id="stok" name="stok" placeholder="Masukkan nominal stok">
                     </div>
                     <div class="form-group">
                         <input type="file" class="form-control form-control-user" id="image" name="image">
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-ban"></i>
-                        Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-ban"></i> Close</button>
                     <button type="submit" class="btn btn-primary"><i class="fas fa-plus-circle"></i> Tambah</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
-<!-- End of Modal Tambah Mneu -->
+<!-- End of Modal Tambah Menu -->
